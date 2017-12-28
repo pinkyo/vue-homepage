a7349b273e415273cde85f640b0b8b4dd843bdda
pinkyo <yinkn@outlook.com>
2017-12-28 21:59:06 +0800

    Others#, add.

---
+++ src/setup/amap-setup.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import VueAMap from 'vue-amap';
+Vue.use(VueAMap);
+
+// 初始化vue-amap
+VueAMap.initAMapApiLoader({
+  // 高德的key
+  key: '9dfcb40559bdebbeaf8fa2279c99e85e',
+  // 插件集合
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
+});
