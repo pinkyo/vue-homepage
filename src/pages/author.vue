826ecfb29620b58902980bf0ca4b8c025805c013
pinkyo <yinkn@outlook.com>
2018-03-30 11:06:59 +0800

    Others#, add.

--- src/pages/author.vue
+++ src/pages/author.vue
@@ -7,7 +7,9 @@
         <div class="resource_item_content">
           <div class="author-pic-div"><img src="../static/author.jpg" ></div>
           <span>
-            Lorem ipsum was conceived as filler text, formatted in a certain way to enable the presentation of graphic elements in documents, without the need for formal copy. Using Lorem Ipsum allows designers to put together layouts and the form of the content before the content has been created, giving the design and production process more freedom.It is widely believed that the history of Lorem Ipsum originates with Cicero in the 1st Century BC and his text De Finibus bonorum et malorum. This philosophical work, also known as On the Ends of Good and Evil, was split into five books. The Lorem Ipsum we know today is derived from parts of the first book Liber Primus and its discussion on hedonism, the words of which had been altered, added and removed to make it nonsensical and improper Latin. It is not known exactly when the text gained its current traditional form. However references to the phrase "lorem ipsum" can be found in the 1914 Loeb Classical Library Edition of the De Finibus in sections 32 and 33.
+            Kainian Yin, a naive coder, love technology and new experience. <b>2016-07</b>, I graduated from SUN-YAT-SEN UNIVERSITY in Mainland China, major is
+            <i>Computer Science And Technology</i>. After graduation, I worked as a intern in Payeco LTD firstly, but I found that it coundn't meet my interest in
+             daily work and I left soon. Then, I get a job chance to work for Ericsson GZ, a department focused on Internet related bussiness.
           </span>
         </div>
       </div>
