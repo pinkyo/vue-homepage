6f0dce65eeb2075167cf22d92b775b4cc852b454
pinkyo <yinkn@outlook.com>
2018-05-01 13:45:55 +0800

    Others#, change page.

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,24 +3,32 @@
     <content-header text="About" />
     <div class="resources">
       <div>
-        <div class="content-sub-header"><h2><i class="fas fa-question" title="Why I do this"></i></h2></div>
+        <div class="content-sub-header"><h2>Orientation</h2></div>
         <div class="resource_item_content content-sub-div">
           <span>
-            I write this site, because I want to try VueJS. I have ever join a project and I got a chance
-            to implement a web page using technical stack including ReactJS, Redux, SCSS and Webpack, since
-            then I start to learn the frontend knowledge. Working with VueJS is just a hobby, and demo like
-            this will be more, visit me in my github page.
+            I'm a professional Java developer, and my main daily work is developing server side function.
+            But, for that my company is one applying Agile managment, and under scrum developing Procedure,
+            I got many chance to take task of front-end. I have jonined a big react application in ABAC for
+            about 6 months, start to learn frontend technology for hobby.
+            VueJS become more and more popular, and I want to try it in a real work. So I developed this site.
           </span>
         </div>
       </div>
       <div>
         <div class="content-sub-header">
-          <h2><i class="fas fa-wrench" title="What I use"></i></h2>
+          <h2>What I use</h2>
         </div>
         <div class="resource_item_content content-sub-div">
           <span>
-            In this demo, I use VueJS + Webpack + VueX + VueRouter + Gaode Map + CSS ....., all is frontend
-            tech.
+            There are manay modules used in my site:
+            <ul>
+                <li>VueJS</li>
+                <li>Vue-Router</li>
+                <li>Element-UI</li>
+                <li>vue-amap</li>
+                <li>vue-i18n</li>
+                <li>vuex...</li>
+            </ul>
           </span>
         </div>
       </div>
